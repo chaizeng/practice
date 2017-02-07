b0bc21082cc272097e8a6f5d1802b8c03227b8b3
kevin <chaizengcn@126.com>
2017-02-07 10:42:48 +0800

    a

--- src/redis/Test.java
+++ src/redis/Test.java
@@ -44,7 +44,7 @@ public class Test {
 	public static void main(String[] args) {
 		Test test = new Test();
 		test.setUp();
-		//test.testString();
+		test.testString();
 		test.testObject();
 		
 		
